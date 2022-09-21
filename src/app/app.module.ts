65979679cc9ea05c0e7b2cd09549293e1edb171f
FernandoGarcia1 <fernandogarciadolores@gmail.com>
2022-09-21 08:54:51 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { FirstModuleModule } from './modules/first-module/first-module.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FirstModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
